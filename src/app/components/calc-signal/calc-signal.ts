194c8df6a1131b2f3684863b1276e89712feeba7
SS1162 <s058329162@gmail.com>
2025-11-05 22:06:01 +0200

    finish calc

--- src/app/components/calc-signal/calc-signal.ts
+++ src/app/components/calc-signal/calc-signal.ts
@@ -1,58 +1,63 @@
-import { Component, signal, Signal,computed, WritableSignal } from '@angular/core';
+import { Component, signal, Signal, computed, WritableSignal } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-calacurtor',
-  standalone:true,
+  selector: '<app-calc-signal>',
+  standalone: true,
   imports: [FormsModule],
-  templateUrl: './calacurtor.html',
-  styleUrl: './calacurtor.scss'
+  templateUrl: './calc-signal.html',
+  styleUrl: './calc-signal.scss'
 })
 export class CalcSignal {
-  num:WritableSignal<number>=signal(0)
-  sum: WritableSignal<number>=signal(0)
-  action: WritableSignal<number>=signal(0)
+  num: WritableSignal<number> = signal(0)
+  sum: WritableSignal<number> = signal(0)
+  action: WritableSignal<number> = signal(0)
+  func: Signal<number> = computed(() => {
+    if (this.action() === 1) {
+      return this.sum()+this.num()
+    }
+    else if (this.action() === 2) {
+      return this.sum()-this.num()
+    }
+    else if (this.action() === 3) {
+    return this.sum()*this.num()
+    }
+    else if (this.action() === 4) {
+     return this.sum()/this.num()
+    }
+    return 0
+  })
 
   funcPlus() {
-  
-    this.action.set(1) 
+    this.sum.set(this.func())
+    this.num.set(0)
+    this.action.set(1)
   }
 
   funcminus() {
-    
-    this.action.set(2) 
+    this.sum.set(this.func())
+    this.num.set(0)
+    this.action.set(2)
   }
 
   funcmult() {
-    
-   this.action.set(3) 
+    this.sum.set(this.func())
+    this.num.set(0)
+    this.action.set(3)
   }
 
   funcdvide() {
-   this.action.set(4) 
+    this.sum.set(this.func())
+    this.num.set(0)
+    this.action.set(4)
   }
-addInput(add:number){
- this.num.set(this.num()*10+add)
-}
-addPoint(){
-   this.num.set(this.num()*100)
-}
-
-  resulte() {
-    if (this.action() === 1) {
-this.sum.set(this.sum() + this.num()) 
-    }
-    else if (this.action() === 2) {
-this.sum.set(this.sum() - this.num())
-    }
-    else if (this.action() === 3) {
-this.sum.set(this.sum() * this.num())
-    }
-    else if (this.action() === 4) {
-this.sum.set(this.sum() + this.num())
-    }
-    this.num = this.sum
+  addInput(add: number) {
+    
+    this.num.set(this.num() * 10 + add)
+  }
+  addPoint() {
+    this.num.set(this.num() * 100)
   }
 }
 
